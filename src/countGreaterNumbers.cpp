7054b079f69d9e4e7151fa7d50ddd2255b5cfebc
naresh117 <naresh117@missionrnd.com>
2016-01-30 07:33:33 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,7 +13,7 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+#include<stdio.h>
 struct transaction {
 	int amount;
 	char date[11];
@@ -21,5 +21,45 @@ struct transaction {
 };
 
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
+	if(Arr==NULL||len<=0||date==NULL)
 	return -1;
+	else
+        {
+            int i,dayc,monc,yearc,counti=0;
+            int year=(date[6]-'0')*1000+(date[7]-'0')*100+(date[8]-'0')*10+(date[9]-'0');
+            int mon=(date[3]-'0')*10+(date[4]-'0');
+            int day=(date[0]-'0')*10+(date[1]-'0');
+            printf("%d %d %d",day,mon,year);
+            for(i=0;i<len;i++)
+            {
+                 yearc=(Arr[i].date[6]-'0')*1000+(Arr[i].date[7]-'0')*100+(Arr[i].date[8]-'0')*10+(Arr[i].date[9]-'0');
+                if(yearc>year)
+                {
+                    counti=len-i;
+                    break;
+                }
+                else if(yearc==year)
+                {
+                     monc=(Arr[i].date[3]-'0')*10+(Arr[i].date[4]-'0');
+
+                    if(monc>mon)
+                    {
+                        counti=len-1;
+                        break;
+                    }
+                    else if(monc==mon)
+                    {   dayc=(Arr[i].date[0]-'0')*10+(Arr[i].date[1]-'0');
+                        if(dayc>day)
+                        {
+                            counti=len-i;
+                        }
+
+                    }
+                }
+            }
+
+        return counti;
+        }
+
 }
+
